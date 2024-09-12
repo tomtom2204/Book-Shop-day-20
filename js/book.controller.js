ef82ec1abda9782e42565e1e39a7498e70cd5a20
tomtom2204 <tomtom2204@gmail.com>
2024-09-12 17:36:58 +0300

    Add removing a book funcs

--- js/book.controller.js
+++ js/book.controller.js
@@ -5,5 +5,31 @@ function onInit(){
 }
 
 function render(){
-    
+    const elBookTable = document.querySelector('.book-tbl')
+    const books = gBooks
+    var strHtmls = ` <tr>
+                        <th>Title</th>
+                        <th>Price</th>
+                        <th>Actions</th>
+                    </tr>`
+    strHtmls += books.map(book => `
+        <tr>
+                <td>${book.title}</td>
+                <td>${book.price}</td>
+                <td>
+                    <button class="read" onclick="onReadClick('${book.id}')">Read</button>
+                    <button class="update" onclick="onUpdateClick('${book.id}')">Update</button>
+                    <button class="delete" onclick="onRemoveBook('${book.id}')">Delete</button>
+                </td>
+        </tr>
+`).join('')
+
+elBookTable.innerHTML = strHtmls
+
+
+}
+
+function onRemoveBook(bookId){
+    removeBook(bookId)
+    render()
 }
--- js/services/book.service.js
+++ js/services/book.service.js
@@ -1,24 +1,30 @@
 'use strict'
 
+var gBooks = getBooks()
 function getBooks() {
     return [
         {
-            id: bg4J78,
+            id: 'bg4J78',
             title: 'The adventures of Lori Ipsi',
             price: 120,
             imgUrl: 'lori-ipsi.jpg'
         },
         {
-            id: bg4J78,
-            title: 'The adventures of Lori Ipsi',
-            price: 120,
+            id: 'bg4J79',
+            title: 'World Atlas',
+            price: 300,
             imgUrl: 'lori-ipsi.jpg'
         },
         {
-            id: bg4J78,
-            title: 'The adventures of Lori Ipsi',
-            price: 120,
+            id: 'bg4J81',
+            title: 'Zorba the Greek',
+            price: 87,
             imgUrl: 'lori-ipsi.jpg'
         }
     ]
+}
+
+function removeBook(bookId){
+    const idx = gBooks.findIndex(book => book.id === bookId)
+    gBooks.splice(idx, 1)
 }
