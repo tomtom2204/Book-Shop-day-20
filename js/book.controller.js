9b5744e937f7ae3d236a59c4580ee3f388e0b0fc
tomtom2204 <tomtom2204@gmail.com>
2024-09-14 22:00:47 +0300

    Allow the user to change the display from a table to a grid of cards. Save the user's preferred display layout to local storage and use it to render the app accordingly when it is loaded

--- js/book.controller.js
+++ js/book.controller.js
@@ -1,21 +1,52 @@
 'use strict'
 
 var gFilterBy = ''
+var gLayout = ''
 
-function onInit(){
+function onInit() {
+    gLayout = loadFromStorage(LAYOUT_KEY)
     render()
 }
 
-function render(){
-    const elBookTable = document.querySelector('.book-tbl')
+
+function render() {
+    const toggleButton = document.querySelector('.toggleLayout')
+    toggleButton.textContent = gLayout ? gLayout : 'Switch to Grid'
     const books = getBooks(gFilterBy)
-    var strHtmls = ` <tr>
+    const elBookCards = document.querySelector('.card-area')
+    const elBookTable = document.querySelector('.book-tbl')
+    elBookCards.innerHTML = ""
+    elBookTable.innerHTML = ""
+    
+    var strHtmls = ``
+
+    if (toggleButton.textContent === 'Switch to Table') {
+        document.querySelector('.card-area').style.gridTemplateColumns = `repeat(${books.length}, 1fr)`
+        
+        strHtmls += !books.length ? `<p>No matching books were found</p>` : books.map(book =>
+            `<div class="card" style="grid-column: ${book.id+1}; grid-row: 1;">
+                        <img src="${book.imgUrl}" alt="">
+                        <div class="overlay">
+                            <h3>${book.title}</h3>
+                            <p>$${book.price}</p>
+                           <!-- <a href="#">BUY NOW</a> -->
+                           <div class="button-group">
+                                <a href="#" class="read" onclick="onShowBookDetails(${book.id})">Read</a>
+                                <a href="#" class="update" onclick="onRenderUpdateModal(${book.id})">Update</a>
+                                <a href="#" class="delete" onclick="onRemoveBook(${book.id})">Delete</a>
+                            </div>
+                        </div>
+                    </div>`
+        ).join('')
+        elBookCards.innerHTML = strHtmls
+    } else {
+        strHtmls += ` <tr>
                         <th>Title</th>
                         <th>Price</th>
                         <th>Actions</th>
                     </tr>`
-    
-    strHtmls += !books.length ? `<tr> <td colspan="3"  class="no-matching">No matching books were found</td></tr>` : books.map(book => `
+
+        strHtmls += !books.length ? `<tr> <td colspan="3"  class="no-matching">No matching books were found</td></tr>` : books.map(book => `
         <tr>
                 <td>${book.title}</td>
                 <td>${book.price}</td>
@@ -24,12 +55,11 @@ function render(){
                     <button class="update" onclick="onRenderUpdateModal(${book.id})">Update</button>
                     <button class="delete" onclick="onRemoveBook(${book.id})">Delete</button>
                 </td>
-        </tr>
-`).join('')
+        </tr>`).join('')
+        elBookTable.innerHTML = strHtmls
+    }
 
-elBookTable.innerHTML = strHtmls
-
-renderStats()
+    renderStats()
 }
 
 function renderStats() {
@@ -47,32 +77,32 @@ function renderStats() {
 }
 
 
-function onRemoveBook(bookId){
+function onRemoveBook(bookId) {
     onActionSuccessful(bookId, 'deleted')
     removeBook(bookId)
     render()
-    
+
 }
 
-function onRenderUpdateModal(bookId){
-    var innerHTML =`<div>
+function onRenderUpdateModal(bookId) {
+    var innerHTML = `<div>
                     <input type="text" class="price-input" placeholder="Enter new price">
                     </div>
                     <button onclick="onUpdateBook(${bookId})">Update</button>`
     showModal(innerHTML)
 }
 
-function onUpdateBook(bookId){
-   var elPriceInput = document.querySelector(".price-input")
-    updatePrice(bookId,elPriceInput.value)
+function onUpdateBook(bookId) {
+    var elPriceInput = document.querySelector(".price-input")
+    updatePrice(bookId, elPriceInput.value)
     closeModal()
     render()
     onActionSuccessful(bookId, 'updated')
 }
 
 
-function onRenderAddBook(){
-    var innerHTML =`<div>
+function onRenderAddBook() {
+    var innerHTML = `<div>
                     <input type="text" class="title-input" placeholder="Enter new book title">
                    </div>
                    <div>
@@ -82,10 +112,10 @@ function onRenderAddBook(){
     showModal(innerHTML)
 }
 
-function onAddBook(){
+function onAddBook() {
     var elTitleInput = document.querySelector(".title-input")
     var elPriceInput = document.querySelector(".price-input")
-    if(!elTitleInput.value.trim() || !elPriceInput.value.trim()) return
+    if (!elTitleInput.value.trim() || !elPriceInput.value.trim()) return
     var bookId = addBook(elTitleInput.value, elPriceInput.value)
     closeModal()
     render()
@@ -100,14 +130,14 @@ function onShowBookDetails(bookId) {
     showModal(innerHTML)
 }
 
-function showModal(innerHTML){
+function showModal(innerHTML) {
     const elModal = document.querySelector('.book-modal')
     const elDetails = elModal.querySelector('.book-modal-container')
     elDetails.innerHTML = innerHTML
     elModal.showModal()
 }
 
-function closeModal(){
+function closeModal() {
     const elModal = document.querySelector('.book-modal')
     elModal.close()
 }
@@ -117,15 +147,22 @@ function onFilterBy(filterInput) {
     render()
 }
 
-function onClearFilter(){
+function onClearFilter() {
     const elFilter = document.querySelector('.filter-input')
-    elFilter.value =''
-    onFilterBy(elFilter.value)    
+    elFilter.value = ''
+    onFilterBy(elFilter.value)
 }
 
 function onActionSuccessful(bookId, action) {
     const book = getBookById(bookId)
     var innerHTML = `<h3 class="msg">The book: ${book.title} was ${action} successfully!</h3>`
     showModal(innerHTML)
-    setTimeout(closeModal,2000)
+    setTimeout(closeModal, 2000)
+}
+
+function changeLayout(button){
+    button.textContent = button.textContent === 'Switch to Grid' ? 'Switch to Table' : 'Switch to Grid'
+    saveLayout(button.textContent)
+    gLayout = button.textContent
+    render() 
 }
--- js/services/book.service.js
+++ js/services/book.service.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const BOOKS_KEY = 'books'
-
+const LAYOUT_KEY = 'layout'
 var gBooks = []
 var gNextId = 0
 _createBooks()
@@ -26,26 +26,6 @@ function getBookById(bookId) {
     return gBooks.find(book => book.id === bookId)
 }
 
-// Private functions
-
-function _createBooks() {
-    gBooks = loadFromStorage(BOOKS_KEY)
-
-    console.log(gBooks)
-    if(gBooks && gBooks.length !== 0){ 
-        console.log(gBooks)
-        gNextId = gBooks[gBooks.length-1].id+1
-        return
-    }
-
-    gBooks = []
-    addBook('The adventures of Lori Ipsi', 120)
-    addBook('World Atlas', 300)
-    addBook('Zorba the Greek', 87)
-    _saveBooks()
-}
-
-
 function addBook(title, price){
     var bookId = gNextId++
     gBooks.push(
@@ -59,13 +39,8 @@ function addBook(title, price){
     return bookId
 }
 
-function _saveBooks() {
-    saveToStorage(BOOKS_KEY, gBooks)
-}
-
 function getBooks(filterBy) {           // List
     if(!filterBy) return gBooks
-
     return gBooks.filter(book => book.title.toLowerCase().startsWith(filterBy.toLowerCase()))
 }
 
@@ -83,3 +58,33 @@ function getCheapBooks() {
     const cheapBooks = gBooks.filter(book => book.price <= 80)
     return cheapBooks.length
 }
+
+
+function saveLayout(userPreference) {
+    saveToStorage(LAYOUT_KEY, userPreference)
+}
+
+// Private functions
+function _createBooks() {
+    gBooks = loadFromStorage(BOOKS_KEY)
+
+    if(gBooks && gBooks.length !== 0){ 
+        gNextId = gBooks[gBooks.length-1].id+1
+        return
+    }
+
+    gBooks = []
+    addBook('The adventures of Lori Ipsi', 120)
+    addBook('World Atlas', 300)
+    addBook('Zorba the Greek', 87)
+    _saveBooks()
+}
+
+function _saveBooks() {
+    saveToStorage(BOOKS_KEY, gBooks)
+}
+
+
+
+
+
